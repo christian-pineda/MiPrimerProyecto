dce4696edef85da35375fdc5f1de63ba5271918c
ChristianPineda <krictian1103@gmial.com>
2026-01-12 02:38:36 -0600

    priemr version de FT Optix

---
+++ ProjectFiles/NetSolution/TimeShelveLogic.cs
@@ -0,0 +1,38 @@
+#region Using directives
+using UAManagedCore;
+using FTOptix.UI;
+using FTOptix.NetLogic;
+using FTOptix.SerialPort;
+using FTOptix.EventLogger;
+using FTOptix.ODBCStore;
+using FTOptix.WebUI;
+using FTOptix.Report;
+#endregion
+
+public class TimeShelveLogic : BaseNetLogic
+{
+    public override void Start()
+    {
+        presetTimeButton = Owner.Get<ToggleButton>("TimedShelve/Layout/DurationButtonsLayout/PresetShelveDurationButton");
+        customTimeButton = Owner.Get<ToggleButton>("TimedShelve/Layout/DurationButtonsLayout/CustomTime/CustomTimeShelveButton");
+
+        PresetShelveDurationButtonPressed();
+    }
+
+    [ExportMethod]
+    public void PresetShelveDurationButtonPressed()
+    {
+        presetTimeButton.Active = true;
+        customTimeButton.Active = false;
+    }
+
+    [ExportMethod]
+    public void CustomTimeShelveButtonPressed()
+    {
+        presetTimeButton.Active = false;
+        customTimeButton.Active = true;
+    }
+
+    private ToggleButton presetTimeButton;
+    private ToggleButton customTimeButton;
+}
